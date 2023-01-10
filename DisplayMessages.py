10b29c164d527f7f491939d81ad3a8c55126d3a2
Sheilla-Chomba <chombasheila980@gmail.com>
2023-01-10 08:36:50 +0300

    This is a trial file to confirm that Pycharm and Git are connected

---
+++ DisplayMessages.py
@@ -0,0 +1,6 @@
+# Below is the programming exercise
+# (Display three different messages) Write a program that displays Welcome to
+# Python, Welcome to Computer Science, and Programming is fun.
+print("Welcome to Python")
+print("Welcome to Computer Science")
+print("Programming is Fun")
