ad7a9c654600a6edd605e3e0a774365ab27da432
Sheilla-Chomba <chombasheila980@gmail.com>
2023-01-10 08:41:47 +0300

    This is a trial file to confirm that Pycharm and Git are connected

---
+++ DisplayMessage5times.py
@@ -0,0 +1,8 @@
+# Below is the programming exercise
+# (Display the same message five times) Write a program that displays Welcome to
+# Python five times.
+print("Welcome to Python")
+print("Welcome to Python")
+print("Welcome to Python")
+print("Welcome to Python")
+print("Welcome to Python")
