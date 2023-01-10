ed005f56e611841a510a755a3eb646a5ac980eff
Sheilla-Chomba <chombasheila980@gmail.com>
2023-01-10 10:42:34 +0300

    Exercise 1.9

---
+++ Introduction to Python/Ex1.9AreaAndPerimeterRectangle.py
@@ -0,0 +1,5 @@
+# Below is the programming exercise
+# (Area and perimeter of a rectangle) Write a program that displays the area and
+# perimeter of a rectangle with the width of 4.5 and height of 7.9
+
+print("The area of the rectangle is ",4.5*7.9,"\nThe perimeter of the rectangle is ",(4.5+7.9)*2)
