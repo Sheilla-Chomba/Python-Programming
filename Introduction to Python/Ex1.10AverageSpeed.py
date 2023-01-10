171a9934e113a3a796b733ed72cb082372441d46
Sheilla-Chomba <chombasheila980@gmail.com>
2023-01-10 10:49:42 +0300

    Exercise 1.10

---
+++ Introduction to Python/Ex1.10AverageSpeed.py
@@ -0,0 +1,6 @@
+# Below is the programming exercise
+# (Average speed) Assume a runner runs 14 kilometers in 45 minutes and 30 seconds.
+# Write a program that displays the average speed in miles per hour. (Note that
+# 1 mile is 1.6 kilometers.)
+
+print("The average speed in Miles/hr is ",(14/1.6)/(((45*60)+30)/3600))
