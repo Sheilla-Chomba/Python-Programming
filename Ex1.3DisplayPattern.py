ed9c552302f6d5300885eadad43b328d2f78c165
Sheilla-Chomba <chombasheila980@gmail.com>
2023-01-10 09:02:38 +0300

    Ex1.4 and Ex1.3

---
+++ Ex1.3DisplayPattern.py
@@ -0,0 +1,7 @@
+# Below is the programming exercise
+# (Display a pattern) Write a program that displays the following pattern:
+print("FFFFFF  U     U   NN    NN")
+print("FF      U     U   NNN   NN")
+print("FFFFFF  U     U   NN N  NN")
+print("FF       U   U    NN  N NN")
+print("FF        UUU     NN   NNN")
