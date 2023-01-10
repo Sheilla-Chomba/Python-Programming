35c91a733db25275dee7daea9529ae7d160c2c7c
Sheilla-Chomba <chombasheila980@gmail.com>
2023-01-10 10:09:33 +0300

    Exercise 1.5

---
+++ Ex1.5Compute Expressions.py
@@ -0,0 +1,5 @@
+# Below is the programming exercise
+# (Compute expressions) Write a program that displays the result of
+# (9.5 * 4.5 - 2.5 * 3)/(45.5 - 3.5)
+
+print(((9.5 * 4.5) - (2.5 * 3))/(45.5 - 3.5))
