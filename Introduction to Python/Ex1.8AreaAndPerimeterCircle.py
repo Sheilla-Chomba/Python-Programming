e635a65184e965ae16403f044c3f5f728b30fc96
Sheilla-Chomba <chombasheila980@gmail.com>
2023-01-10 10:34:46 +0300

    Exercise 1.8

---
+++ Introduction to Python/Ex1.8AreaAndPerimeterCircle.py
@@ -0,0 +1,6 @@
+# Below is the programming exercise
+# (Area and perimeter of a circle) Write a program that displays the area and
+# perimeter of a circle that has a radius of 5.5
+
+print("The area of the circle is ", 5.5*5.5*3.142)
+print("The perimeter of the circle is ",3.142*5.5*2)
