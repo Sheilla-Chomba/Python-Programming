ad9aea64b203efff1921114171bd6ed400db2ad4
Sheilla-Chomba <chombasheila980@gmail.com>
2023-01-12 08:43:05 +0300

    Exercise 2.3

---
+++ Elementary Programming/Ex2.3.py
@@ -0,0 +1,12 @@
+# Below is the programming exercise
+# (Convert feet into meters) Write a program that reads a number in feet, converts it
+# to meters, and displays the result.
+
+# Prompts user to type in length in feet
+feet=float(input("Enter the lenth in feet: "))
+
+# Converts length in feet to metres
+metres=feet*0.305
+
+# prints the results of the conversion
+print("The length in metres is ",metres)
