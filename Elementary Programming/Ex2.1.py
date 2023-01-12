63165754ffcad04564942041e5953ac8a66cd3a4
Sheilla-Chomba <chombasheila980@gmail.com>
2023-01-12 08:17:37 +0300

    Exercise 2.1

---
+++ Elementary Programming/Ex2.1.py
@@ -0,0 +1,12 @@
+# Below is the programming exercise
+# (Convert Celsius to Fahrenheit) Write a program that reads a Celsius degree from
+# the console and converts it to Fahrenheit and displays the result.
+
+# Prompts user to type the level of temperature in Celcius Degree
+celcius=int(input("Enter the temperature in Celcius Degree: "))
+
+# Introduction of a new variable Fahrenheit and the formula to get it
+fahrenheit=(9/5)*celcius+32
+
+# Prints out the value of the temperature in Fahrenheit
+print("The temperature in Fahrenheit is ",fahrenheit)
