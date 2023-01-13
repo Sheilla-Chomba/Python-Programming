295939b14f554281b45c782e74d414e15097f873
Sheilla-Chomba <chombasheila980@gmail.com>
2023-01-13 09:26:51 +0300

    Exercise 2.12

---
+++ Elementary Programming/Ex 2.12.py
@@ -0,0 +1,10 @@
+# Below is the programming exercise
+# (Print a table) Write a program that displays the following table:
+
+# This prints the table that is required
+print("a    b   a**b")
+print("1    2   ",1**2)
+print("2    3   ",2**3)
+print("3    4   ",3**4)
+print("4    5   ",4**5)
+print("5    6   ",5**6)
