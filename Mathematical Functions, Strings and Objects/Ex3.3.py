6fa08193fea7daa65ed1026bf1c6ea1dba43426f
Sheilla-Chomba <chombasheila980@gmail.com>
2023-01-26 15:56:25 +0300

    Exercise 3.3

--- Mathematical Functions, Strings and Objects/Ex3.3.py
+++ Mathematical Functions, Strings and Objects/Ex3.3.py
@@ -24,3 +24,13 @@ distSavAt=6371.01 * math.acos(math.sin(math.radians(SVlat)) * math.sin(math.radi
 distSavChar=6371.01 * math.acos(math.sin(math.radians(SVlat)) * math.sin(math.radians(CHlat)) + math.cos(math.radians(SVlat)) * math.cos(math.radians(CHlat)) * math.cos(math.radians(SVlong - CHlong)))
 distAtChar=6371.01 * math.acos(math.sin(math.radians(ATlat)) * math.sin(math.radians(CHlat)) + math.cos(math.radians(ATlat)) * math.cos(math.radians(CHlat)) * math.cos(math.radians(ATlong - CHlong)))
 
+# This is the formula for area
+side1=(distOrAt+distOrSav+distSavAt)/2
+area1=math.sqrt(side1*(side1-distOrAt)*(side1-distOrSav)*(side1-distSavAt))     # This is the area of the first triangle
+
+side2=(distSavChar+distAtChar+distSavAt)/2
+area2=math.sqrt(side1*(side2-distSavChar)*(side2-distAtChar)*(side2-distSavAt))  # This is the area of the first triangle
+
+totArea=area1+area2
+
+print(f"The area between Atlanta, Savannah, Charlotte and Orlando is ",format(totArea,".2f"))
