b6d6fecdd4b04b81d95f691983e36a6826684f9f
Sheilla-Chomba <chombasheila980@gmail.com>
2023-01-27 11:43:55 +0300

    Exercise 4.3

---
+++ Selections/Ex4.3.py
@@ -0,0 +1,19 @@
+# Below is the programming exercise
+# Write a program that prompts the user to enter a, b, c, d, e, and f and display the
+# result. If ad – bc is 0, report that The equation has no solution.
+
+import math
+# Prompts user to type in the value of a, b, c, d, e and f
+a=eval(input("Type in the value of a: "))
+b=eval(input("Type in the value of b: "))
+c=eval(input("Type in the value of c: "))
+d=eval(input("Type in the value of d: "))
+e=eval(input("Type in the value of e: "))
+f=eval(input("Type in the value of f: "))
+
+if ((a*d)-(b*c))==0:
+    print("The equation has no solution.")
+else:
+    x=((e*d)-(b*f))/((a*d)-(b*c))
+    y=((a*f)-(e*c))/((a*d)-(b*c))
+    print(f"The value of x is {x} and that of y is {y}")
