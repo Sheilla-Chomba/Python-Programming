abf7f10a2277bb7631f093aaa0bd73f9a8a32681
Sheilla-Chomba <chombasheila980@gmail.com>
2023-01-30 13:01:46 +0300

    Exercise 4.16

---
+++ Selections/Ex4.16.py
@@ -0,0 +1,6 @@
+# Below is the programming exercise
+# (Random character) Write a program that displays a random uppercase letter.
+
+import random
+upperLetter=random.randint(65,90)
+print(chr(upperLetter))
