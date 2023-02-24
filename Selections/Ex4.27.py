9f8b1130e705e3bcca93e769d709a5161865d36b
Sheilla-Chomba <chombasheila980@gmail.com>
2023-02-24 16:12:09 +0300

    Exercise 4.27

---
+++ Selections/Ex4.27.py
@@ -0,0 +1,20 @@
+# Below is the programming exercise
+# Write a program that prompts the user to enter a point with
+# x- and y-coordinates and determines whether the point is inside the triangle.
+
+import math
+# Prompts user to type in the points to check whether they are inside the rectangle
+x=eval(input("What is the x coordinate: "))
+y=eval(input("What is the y coordinate: "))
+
+horizontalDistance=math.sqrt(math.pow((x-0),2))
+verticalDistance=math.sqrt(+math.pow((y-0),2))
+
+if verticalDistance<=100:
+    if horizontalDistance<=2*(100-verticalDistance):
+        print(f"Point ({x}, {y}) is in the triangle.")
+    else:
+        print(f"Point ({x}, {y}) is not in the triangle.")
+
+else:
+    print(f"Point ({x}, {y}) is not in the triangle.")
