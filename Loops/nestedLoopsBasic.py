5cefcaeaca593b841ea44ea71b1fa4f57349c37b
Sheilla-Chomba <chombasheila980@gmail.com>
2023-02-27 14:27:23 +0300

    This is a multiplication table using for loops. This is just the basic version

---
+++ Loops/nestedLoopsBasic.py
@@ -0,0 +1,7 @@
+# This program basically displays the multiplication table
+# This is basically the basic version
+
+for i in range(1,10):
+    for j in range(1,10):
+        print(i*j, end="\t")
+    print("")
