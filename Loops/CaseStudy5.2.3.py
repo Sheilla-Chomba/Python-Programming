533e9222301621f6c6cabd4a972b140eb1d9b4cb
Sheilla-Chomba <chombasheila980@gmail.com>
2023-02-27 10:36:34 +0300

    Case Study 5.2.3 This the basic version of the case study

---
+++ Loops/CaseStudy5.2.3.py
@@ -0,0 +1,18 @@
+# Below is a case study for giving users 5 subtraction quizes
+
+from random import randint
+
+questions=5
+while questions>0:
+    num1=randint(0,10)
+    num2=randint(0,10)
+
+    answer=eval(input(f"What is {num1} - {num2} = "))
+
+    if(answer==(num1-num2)):
+        print(f"Your are correct. {answer} is the answer.")
+    else:
+        print(f"That is incorrect. The answer is the {answer}.")
+
+    questions=questions-1
+print("\nThat is the end of your quiz.")
