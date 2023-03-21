ed013f8acac0573ba291e78f08da670af6ba8bea
Sheilla-Chomba <chombasheila980@gmail.com>
2023-03-21 12:50:42 +0300

    Practice exercise

---
+++ Files and Exception Handling/Ex4.py
@@ -0,0 +1,6 @@
+# How to read data from a file using python
+# Wanted to see if python would react differently if I put a special character in the file
+
+notes=open("data.txt")
+data=notes.read()
+print(data)
